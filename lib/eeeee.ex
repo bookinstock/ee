51152f542226d836c8c4374b7327d115c9d17aca
Wende Lu <wendelu1991@gmail.com>
2020-09-24 22:37:23 +0800

    init

---
+++ lib/eeeee.ex
@@ -0,0 +1,18 @@
+defmodule Eeeee do
+  @moduledoc """
+  Documentation for Eeeee.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Eeeee.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/eeeee_test.exs
@@ -0,0 +1,8 @@
+defmodule EeeeeTest do
+  use ExUnit.Case
+  doctest Eeeee
+
+  test "greets the world" do
+    assert Eeeee.hello() == :world
+  end
+end
